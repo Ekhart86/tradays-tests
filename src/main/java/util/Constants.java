697bd371f60b3a729a7f14c771b71e21fb0924e9
Ekhart86 <valderama1@yandex.ru>
2019-09-26 20:44:22 +0300

    rename suite

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -3,7 +3,7 @@ package util;
 public class Constants {
 
     public final static String BASE_APPIUM_URL = "http://127.0.0.1:4723/wd/hub";
-    public final static Integer BASE_IMPLICITLY_WAIT = 20;
+    public final static Integer BASE_IMPLICITLY_WAIT = 25;
     public final static String APP_ACTIVITY = "net.metaquotes.ui.MainActivity";
     public final static String APP_PACKAGE = "net.metaquotes.economiccalendar";
     public final static String UDID = System.getProperty("udid");
